38a5706d22b813db53de5832b35af5fa5c87464e
Marlon <marlonr@hey.com>
2024-10-05 01:25:14 -0400

    Initial Commit

---
+++ NiceCopy/NiceCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceCopyApp.swift
+//  NiceCopy
+//
+//  Created by Marlon Raskin on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct NiceCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
